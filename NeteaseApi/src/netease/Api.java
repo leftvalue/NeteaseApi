19ad672a9b72be73a5c2dd2bcc2c3f189a8f4e9e
Dick <mrdick@foxmail.com>
2017-10-09 13:29:07 +0800

    init
    初始化代码 demo

---
+++ NeteaseApi/src/netease/Api.java
@@ -0,0 +1,40 @@
+package netease;
+
+public class Api {
+    private final static String BaseURL = "http://music.163.com/";
+
+    /**
+     * 获取用户歌单
+     *
+     * @param uid
+     * @return
+     */
+    public static UrlParamPair getPlaylistOfUser(String uid) {
+        UrlParamPair upp = new UrlParamPair();
+        upp.setUrl(BaseURL + "weapi/user/playlist?csrf_token=");
+        upp.addPara("offset", 0);
+        upp.addPara("uid", uid);
+        upp.addPara("limit", 5);
+        upp.addPara("csrf_token", "nothing");
+        return upp;
+    }
+
+    /**
+     * 获取歌单详情
+     *
+     * @param playlist_id
+     * @return
+     */
+    public static UrlParamPair getDetailOfPlaylist(String playlist_id) {
+        UrlParamPair upp = new UrlParamPair();
+        upp.setUrl(BaseURL + "weapi/v3/playlist/detail?csrf_token=");
+        upp.addPara("id", playlist_id);
+        upp.addPara("offset", 0);
+        upp.addPara("total", "True");
+        upp.addPara("limit", 1000);
+        upp.addPara("n", 1000);
+        upp.addPara("csrf_token", "nothing");
+        return upp;
+    }
+    //todo:analyse more api
+}
---
+++ NeteaseApi/src/secret/JSSecret.java
@@ -0,0 +1,54 @@
+package secret;
+
+import jdk.nashorn.api.scripting.ScriptObjectMirror;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+
+public class JSSecret {
+    private static Invocable inv;
+    public static final String encText = "encText";
+    public static final String encSecKey = "encSecKey";
+
+    /**
+     * 从本地加载修改后的 js 文件到 scriptEngine
+     */
+    static {
+        try {
+            Path path = Paths.get("core.js");
+            byte[] bytes = Files.readAllBytes(path);
+            String js = new String(bytes);
+            ScriptEngineManager factory = new ScriptEngineManager();
+            ScriptEngine engine = factory.getEngineByName("JavaScript");
+            engine.eval(js);
+            inv = (Invocable) engine;
+            System.out.println("Init completed");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static ScriptObjectMirror get_params(String paras) throws Exception {
+        ScriptObjectMirror so = (ScriptObjectMirror) inv.invokeFunction("myFunc", paras);
+        return so;
+    }
+
+    public static HashMap<String, String> getDatas(String paras) {
+        try {
+            ScriptObjectMirror so = (ScriptObjectMirror) inv.invokeFunction("myFunc", paras);
+            HashMap<String, String> datas = new HashMap<>();
+            datas.put("params", so.get(JSSecret.encText).toString());
+            datas.put("encSecKey", so.get(JSSecret.encSecKey).toString());
+            return datas;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
